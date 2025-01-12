b786710ad05070bce1c683cf1a52984cba669fc3
Ashborn14 <neon99156@gmail.com>
2025-01-12 18:00:40 +0530

    Update Hero.jsx

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 const Hero = () => {
   return (
-    <div className='flex flex-col items-center text-center md:mx-56 gap-9 h-screen'>
+    <div className='flex flex-col items-center text-center md:mx-56 gap-9 h-screen mx-5'>
         <h1 className='font-extrabold text-[20px] md:text-[50px] leading-relaxed md:text-center mt-16'>
             <span className='text-[#f56551]'>Discover Your Next Adventure with AI:</span> Personalized Itineraries at Your Fingertips</h1>
         <p className='text-gray-500 text-xl text-center'>our personal trip planner and travel curator, creating custom itineraries tailored to your interests and budget.</p>
